aa0280d0b7414d7c9d2043762263dd802b8047de
gsrr <jerrycheng1128@gmail.com>
2015-08-14 07:21:28 +0800

    update

---
+++ Chris/crawler/crawler.py
@@ -0,0 +1,35 @@
+import myLib
+import urllib
+
+
+
+
+def extractID(line):
+    return line.strip().split()[0]
+
+
+def readFile(func):
+    data = []
+    with open("Lien_Data_20150812.txt", "r") as fr:
+        line = fr.readline()
+        while line:
+            data.append(func(line))
+            line = fr.readline()
+    return data
+
+def extractDataFromURL(data):
+    url_temp="http://securities.stanford.edu/filings-case.html?id=%s"
+    for i in range(1, len(data)):
+        url_path = url_temp%(data[i])
+        url_data = myLib.myUrl(url_path)
+        print url_data
+        break
+
+        
+
+def main():
+    data = readFile(extractID);
+    extractDataFromURL(data)
+
+if __name__ == "__main__":
+    main()
